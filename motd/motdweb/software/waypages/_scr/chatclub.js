811439c7659ccf0e8f7eb07fa4201dcd97be35a7
devzspy <aluvshis@gmail.com>
2018-10-07 04:32:39 -0700

    More MOTD Pages + GameSpy Arcade Images

---
+++ motd/motdweb/software/waypages/_scr/chatclub.js
@@ -0,0 +1,58 @@
+function printChatClub() {
+
+document.write('<div id="musthaveHeader">USER ROOM OF THE WEEK</div>');
+document.write('<div id="musthave">');
+document.write('<b><a href="userroom://GameSpy%20Arcade%20Tour">GameSpy Arcade Tour</a></b><br/>');
+document.write('Our man Spiff says, "The GameSpy Arcade Tour room is a great place to find tips on using GameSpy Arcade and to rub elbows with the occasional [HELP]er or Chat Admin."');
+document.write('<br/><br/>');
+document.write(' ');
+document.write('	<!-- ADDITIONAL GAMES -->');
+document.write('	<table border="0" cellpadding="0" cellspacing="2" width="348">');
+document.write('	<tr>');
+document.write('	<td width="264" colspan="3"><b>Spiff\'s Favorite Games</b></td>');
+document.write('	<td width="80" rowspan="3" align="center"><img src="http://web.archive.org/web/20070226220924/http://www.gamespyarcade.com/software/arcadedaily/_img/partyroom/featured/spiff.gif" width="50" height="50"/></td>');
+document.write('	</tr>');
+document.write('	<tr>');
+document.write('	<td width="12"><a href="gsarcade://bfvietnam://GSP!bfvietnam/?type=title"><img src="http://web.archive.org/web/20070226220924/http://www.gamespyarcade.com/software/arcadedaily/_img/index/rocketicon.gif" width="12" height="12" border="0"></a></td>');
+document.write('	<td width="12"><a href="http://web.archive.org/web/20070226220924/http://www.planetbattlefield.com/"><img src="http://web.archive.org/web/20070226220924/http://www.gamespyarcade.com/software/arcadedaily/_img/index/arrowdown.gif" width="12" height="12" border="0"></a></td>');
+document.write('	<td width="274"><a href="gsarcade://bfvietnam://GSP!bfvietnam/?type=title">Battlefield Vietnam</a></td>');
+document.write('	</tr>');
+document.write('	<tr>');
+document.write('	<td width="12"><a href="gsarcade://wotr://GSP!wotr/?type=title"><img src="http://web.archive.org/web/20070226220924/http://www.gamespyarcade.com/software/arcadedaily/_img/index/rocketicon.gif" width="12" height="12" border="0"></a></td>');
+document.write('	<td width="12"><a href="http://web.archive.org/web/20070226220924/http://www.gamespy.com/games/5296.shtml"><img src="http://web.archive.org/web/20070226220924/http://www.gamespyarcade.com/software/arcadedaily/_img/index/arrowdown.gif" width="12" height="12" border="0"></a></td>');
+document.write('	<td width="274"><a href="gsarcade://wotr://GSP!wotr/?type=title">War of the Ring</a></td>');
+document.write('	</tr>');
+document.write('	<tr>');
+document.write('	<td width="12"><a href="gsarcade://bfield1942://GPG!485/?type=group&group=485"><img src="http://web.archive.org/web/20070226220924/http://www.gamespyarcade.com/software/arcadedaily/_img/index/rocketicon.gif" width="12" height="12" border="0"></a></td>');
+document.write('	<td width="12"><a href="http://web.archive.org/web/20070226220924/http://www.fileplanet.com/files/120000/121935.shtml"><img src="http://web.archive.org/web/20070226220924/http://www.gamespyarcade.com/software/arcadedaily/_img/index/arrowdown.gif" width="12" height="12" border="0"></a></td>');
+document.write('	<td width="224"><a href="gsarcade://bfield1942://GPG!485/?type=group&group=485">BF1942: Desert Combat</a></td>');
+document.write('	<td width="80" align="center">[GSI]Spiff</td>');
+document.write('	</tr>');
+document.write('	</table>');
+document.write('	<!-- /ADDITIONAL GAMES -->');
+document.write(' ');
+document.write('</div>');
+document.write('<!-- /MUST HAVE -->');
+
+}
+/*
+     FILE ARCHIVED ON 22:09:24 Feb 26, 2007 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:19:22 Aug 27, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 41.758 (3)
+  esindex: 0.008
+  captures_list: 59.794
+  CDXLines.iter: 12.732 (3)
+  PetaboxLoader3.datanode: 45.52 (4)
+  exclusion.robots: 0.161
+  exclusion.robots.policy: 0.149
+  RedisCDXSource: 1.644
+  PetaboxLoader3.resolve: 241.832
+  load_resource: 256.356
+*/
