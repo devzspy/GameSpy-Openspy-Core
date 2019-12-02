9874c8987829853d98c4864c8ed280af8e46f4cd
devzspy <aluvshis@gmail.com>
2019-12-02 02:16:00 -0700

    Modified portrait upload.
    Still has some issues that needs resolving. However, uploads portrait, resizes portrait.
    
    The placement of the portrait still needs some work / figuring out.
    
    Need to also add the MySQL query to update the DB with the new portrait info

--- motd/motdweb/software/imglib/ppost.php
+++ motd/motdweb/software/imglib/ppost.php
@@ -3,19 +3,59 @@
 include('class.upload.php');
 //ppost.asp?pid=10001&uid=10001&ppass=insert_password&picnum=01000 HTTP/1.1
 
-$pid = $_POST['pid'];
-$uid = $_POST['uid'];
-$password = $_POST['ppass'];
-$picture = $_POST['picnum'];
-$folder = "00000";
+$pid = $_REQUEST['pid'];
+$uid = $_REQUEST['uid'];
+$password = $_REQUEST['ppass'];
+$picture = $_REQUEST['picnum'];
 
-if(isset($_FILES['image'])){
+/*
+10001 = /0/10/1/
+492845 = /0/492/845/
+85675663 = /85/675/663
+*/
+
+if(strlen($pid) <= 3) {
+   $temp = str_split($pid);
+   $folder = join("/", $temp);
+}
+else if(strlen($pid) <= 4) {
+  $temp = str_split($pid);
+  $folder = join("", array_slice($temp,0,1))."/".join("",array_slice($temp,1,3));
+}
+else if(strlen($pid) <= 5) {
+  $temp = str_split($pid);
+  $folder = join("", array_slice($temp,0,2))."/".join("",array_slice($temp,2,3));
+}
+else if(strlen($pid) <= 6) {
+  $temp = str_split($pid);
+  $folder = join("", array_slice($temp,0,3))."/".join("",array_slice($temp,3,3));
+}
+else if(strlen($pid) <= 7) {
+  $temp = str_split($pid);
+  $folder = join("", array_slice($temp,0,1))."/".join("",array_slice($temp,1,3))."/".join("",array_slice($temp,4,3));
+}
+else if(strlen($pid) <= 8) {
+  $temp = str_split($pid);
+  $folder = join("", array_slice($temp,0,2))."/".join("",array_slice($temp,2,3))."/".join("",array_slice($temp,5,3));
+}
+else if(strlen($pid) <= 9) {
+  $temp = str_split($pid);
+  $folder = join("", array_slice($temp,0,3))."/".join("",array_slice($temp,3,3))."/".join("",array_slice($temp,6,3));
+}
+else if(strlen($pid) <= 10) {
+  $temp = str_split($pid);
+  $folder = join("", array_slice($temp,0,1))."/".join("", array_slice($temp,1,3))."/".join("",array_slice($temp,4,3))."/".join("",array_slice($temp,7,3));
+}
+
+if(isset($_FILES['FILE1'])){
   $errors= array();
-  $file_name = $_FILES['image']['name'];
-  $file_size = $_FILES['image']['size'];
-  $file_tmp = $_FILES['image']['tmp_name'];
-  $file_type = $_FILES['image']['type'];
-  $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+  $file_name = $_FILES['FILE1']['name'];
+  $file_size = $_FILES['FILE1']['size'];
+  $file_tmp = $_FILES['FILE1']['tmp_name'];
+  $file_type = $_FILES['FILE1']['type'];
+  $fname = strtolower(reset(explode('.',$_FILES['FILE1']['name'])));
+  $file_ext=strtolower(end(explode('.',$_FILES['FILE1']['name'])));
+  $full_new_file = $fname.".jpg";
   
   $expensions= array("jpeg","jpg","png", "gif","tga","bmp","tif","pcx");
   
@@ -27,19 +67,19 @@ if(isset($_FILES['image'])){
      $errors[]='File size must be exactly 2 MB';
   }
 
-  if (!is_dir("/var/www/software/imglib/portraits/$folder")) {
-    mkdir("/var/www/software/imglib/portraits/$folder", 0777, true);
+  if (!is_dir("/var/www/software/imglib/portraits/user/$folder")) {
+    mkdir("/var/www/software/imglib/portraits/user/$folder", 0777, true);
 }
   
   if(empty($errors)==true) {
-     move_uploaded_file($file_tmp,"/var/www/software/imglib/portraits/$folder/".$file_name);
-     echo "Success";
-  }else{
-     print_r($errors);
-  }
+     move_uploaded_file($file_tmp,"/var/www/software/imglib/portraits/user/$folder/".$full_new_file);
+     print "success";
+  }#else{
+  #   print_r($errors);
+  #}
 }
 
-$image = "./portraits/$folder/$file_name";
+$image = "./portraits/user/$folder/$full_new_file";
 
 $handle = new upload($image);
 if ($handle->uploaded) {
@@ -47,9 +87,9 @@ if ($handle->uploaded) {
   $handle->image_resize         = true;
   $handle->image_x              = 96;
   $handle->image_ratio_y        = 96;
-  $handle->process('./portraits/'.$folder.'/');
+  $handle->process('./portraits/user/'.$folder.'/');
   if ($handle->processed) {
-    echo 'image resized';
+    #echo 'image resized';
     $handle->clean();
   } else {
     echo 'error : ' . $handle->error;
